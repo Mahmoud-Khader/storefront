cdeed6b820344058fb97edef4573e323b2e480a4
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-10-04 21:13:47 +0300

    lab37

--- src/components/header.js
+++ src/components/header.js
@@ -9,8 +9,10 @@ import MenuItem from "@material-ui/core/MenuItem";
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import { makeStyles } from '@material-ui/core/styles';
+import { showCart } from "../store/actions";
+import { connect } from "react-redux";
 
-export default function Header() {
+ function Header() {
     return (
         <>
           <ButtonAppBar />
@@ -28,7 +30,7 @@ export default function Header() {
         flexGrow: 1,
       },
     }));
-    function ButtonAppBar() {
+    function ButtonAppBar(props) {
         const classes = useStyles();
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -49,7 +51,7 @@ export default function Header() {
           </Typography>
           <MenuItem>
         <IconButton aria-label="show 2 new notifications" color="inherit">
-          <Badge  color="secondary">
+          <Badge onClick={()=>{props.showCart(!props.cart.show)}} badgeContent={props.cart.count}  color="secondary">
             <ShoppingCartIcon style={{marginRight:'20px'}} />
           </Badge>
         </IconButton>
@@ -60,3 +62,9 @@ export default function Header() {
     </Box>
   );
 }
+
+const mapStateToprops = (state) => {
+  return { cart: state.cartReducer };
+};
+const mapDispatchToProps = { showCart };
+export default connect(mapStateToprops,mapDispatchToProps)(ButtonAppBar,Header);
--- src/store/product.js
+++ src/store/product.js
@@ -1,121 +1,123 @@
 const initialState = {
-    categories: [
-      {
-        displayName: 'FOOD',
-        normalizedName: 'food',
-        description: 'FOOD Category'
-      },
-      {
-        displayName: 'ELECTRONICS',
-        normalizedName: 'elecronics',
-        description: 'Elecronics Category'
-      },
-      {
-        displayName: 'VIDEO GAMES',
-        normalizedName: 'video games',
-        description: 'Video Games Category'
-      },
-    ],
     product:[
       {
         category:'FOOD',
-        name:'food',
-        description:'Mansaf',
+        name:'Mansaf',
+        description:'Jordinain tradisional food ',
         price:"25$",
-        inventoryCount:'Infinit',
+        inventoryCount:100,
         image:'https://cdn.theculturetrip.com/wp-content/uploads/2017/03/jordanian-mansaf-topped-with-herbs-1024x721.jpg'
       },
       {
         category:'FOOD',
-        name:'food',
-        description:'Pizza',
+        name:'Pizza',
+        description:'Cheese pizza',
         price:"2.5$",
-        inventoryCount:'500',
+        inventoryCount:500,
         image:'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_5-SQjpg.jpg'
       },
       {
         category:'FOOD',
-        name:'food',
-        description:'Burger',
+        name:'Burger',
+        description:'BEEF Burger',
         price:"2.5$",
-        inventoryCount:'1000',
+        inventoryCount:1000,
         image:'https://img.etimg.com/thumb/width-640,height-480,imgsize-951452,resizemode-1,msid-66325015/industry/cons-products/food/burger-king-leaves-starbucks-behind/1.jpg'
       },
       {
         category:'ELECTRONICS',
-        name:'elecronics',
-        description:'Iphone11',
+        name:'Iphone11',
+        description:'Brand New Phone',
         price:"900$",
-        inventoryCount:'100',
+        inventoryCount:100,
         image:'https://i.insider.com/5d7808702e22af12a2793d58?width=1200&format=jpeg'
       },
       {
         category:'ELECTRONICS',
-        name:'elecronics',
-        description:'Gaming PC',
+        name:'Gaming PC',
+        description:'Super Fast PC',
         price:"1500$",
-        inventoryCount:'50',
+        inventoryCount:50,
         image:'https://cdn.mos.cms.futurecdn.net/U5Fdom8yDwVcEivFjiqajF.jpg'
       },
       {
         category:'ELECTRONICS',
-        name:'elecronics',
-        description:'LabTop',
+        name:'LabTop',
+        description:'Gaming Labtop',
         price:"1000$",
-        inventoryCount:'25',
+        inventoryCount:25,
         image:'https://m.media-amazon.com/images/I/61NfcHUmxBL._AC_SL1000_.jpg'
       },
       {
         category:'VIDEO GAMES',
-        name:'video games',
-        description:'World Of Warcraft',
+        name:'World Of Warcraft',
+        description:'RPG Game',
         price:"40$",
-        inventoryCount:'Infinit',
+        inventoryCount:9999,
         image:'https://cdn-cf.gamivo.com/image_original.jpg?f=119652&n=590ef020-fe2f-11e9-ba7b-1d746b5ec20a.jpg&h=0d24fbe66609206cd15bb856d8452fee'
       },
       {
         category:'VIDEO GAMES',
-        name:'video games',
-        description:'Dota 2',
+        name:'Dota 2',
+        description:'Multiplayer Game 5 vs 5',
         price:"For Free",
-        inventoryCount:'Infinit',
+        inventoryCount:9999,
         image:'https://i2.wp.com/laptopmedia.com/wp-content/uploads/2020/11/1599845493_dota-2-russia.png?ssl=1'
       },
       {
         category:'VIDEO GAMES',
-        name:'video games',
-        description:'Red Dead Redemption',
+        name:'Red Dead Redemption',
+        description:'Awsome Reality Game',
         price:"60$",
-        inventoryCount:'Infinit',
+        inventoryCount:99999,
         image:'https://i.ytimg.com/vi/F63h3v9QV7w/maxresdefault.jpg'
       }
       
     ],
-    activeCategory: '',
+    activeProduct: [],
   }
   
   
-  function productsReducer(state = initialState, action) {
-  
+  const productsReducer = (state = initialState, action) => {
     const { type, payload } = action;
   
     switch (type) {
-      case 'ACTIVE_CATEGORY':
-        return { ...state, activeCategory: payload }
-        default:
-          return state;
-    };
-  }
+      case "ACTIVE":
+       state.activeProduct=state.product.filter(item=>{
+         return (payload===item.category ? item.category:null)
+       })
+       console.log(state.activeProduct);
+        return state;
   
   
-  // action
+      case "ADDPRODUCT":
+        state.activeProduct = state.activeProduct.map((item) => {
+          if (item.name === payload.name) {
+            if (item.inventoryCount > 0) {
+              item.inventoryCount = item.inventoryCount - 1;
+            }
+            return item;
+          }
+          return item;
+        });
+        return {...state};
   
-  export const selectCategory = (category) => {
-    return {
-      type: 'ACTIVE_CATEGORY',
-      payload: category
+      case "DELETE":
+        state.activeProduct = state.activeProduct.map((item) => {
+          if (item.name === payload.product.name) {
+            item.inventoryCount = item.inventoryCount + 1;
+  
+            return item;
+          }
+          return item;
+        });
+        return {...state};
+  
+      default:
+        return state;
     }
   };
+  
   
   
   
